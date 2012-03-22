4076a7812709bd783477de851bd6c543525d0b58
Stuart Lodge <lodge.stuart@gmail.com>
2012-03-22 09:47:45 +0000

    black

--- Cirrious.Conference.UI.Touch/Views/TabBarController.cs
+++ Cirrious.Conference.UI.Touch/Views/TabBarController.cs
@@ -35,7 +35,7 @@ namespace Cirrious.Conference.UI.Touch.Views
         private UIViewController CreateTabFor(string title, string imageName, IMvxViewModel viewModel)
         {
             var controller = new UINavigationController();
-			controller.NavigationBar.BackgroundColor = UIColor.Black;
+			controller.NavigationBar.TintColor = UIColor.Black;
             var screen = this.CreateViewControllerFor(viewModel) as UIViewController;
             SetTitleAndTabBarItem(screen, title, imageName);
             controller.PushViewController(screen, false);
