927f166fa6250658e2775b86cdfe81ab4e944cce
Stuart <me@slodge.com>
2012-05-20 08:35:08 -0700

    WinRT UI - solution only - Views not yet implemented! I think they call it a "WIP"!

---
+++ Cirrious.Conference.UI.WinRT/Setup.cs
@@ -0,0 +1,21 @@
+﻿using Cirrious.MvvmCross.Application;
+using Cirrious.MvvmCross.WinRT.Platform;
+using Windows.UI.Xaml.Controls;
+
+namespace Cirrious.Conference.UI.WinRT
+{
+    public class Setup
+        : MvxBaseWinRTSetup
+    {
+        public Setup(Frame rootFrame)
+            : base(rootFrame)
+        {
+        }
+
+        protected override MvxApplication CreateApp()
+        {
+            var app = new Core.NoSplashScreenConferenceApp();
+            return app;
+        }
+    }
+}
